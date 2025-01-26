084d5b8ed406d33b4625412cd7ddbe52dc562cdb
Nonpawit Silabumrungrad <nonpawit.sila@gmail.com>
2025-01-27 05:03:17 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+<<<<<<< Updated upstream
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home'
 import Folder from './pages/Folder'
 function App() {
+=======
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NotFound from './pages/NotFound';
+import Dashboard from './pages/Dashboard';
+import Navbars from './components/Navbars'
+import FarmerFolder from './pages/FarmerFolder';
+import FolderDetail from './pages/FolderDetail';
+import Login from './pages/Login';
+
+const App = () => {
+>>>>>>> Stashed changes
   return (
     <Router>
       <Routes>
